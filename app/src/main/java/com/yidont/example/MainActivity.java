1c04b3c5f9ada73dc73eb77fdaf8e3857ed4199f
can <376762087@qq.com>
2016-12-13 15:46:52 +0800

    测试push

--- app/src/main/java/com/yidont/example/MainActivity.java
+++ app/src/main/java/com/yidont/example/MainActivity.java
@@ -1,9 +1,13 @@
 package com.yidont.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.yidont.example.test.HeadTestActivity;
 
 import view.HeadToolBar;
 
@@ -18,5 +22,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         headToolBar = (HeadToolBar) findViewById(R.id.head_toolbar);
         headToolBar.setBackgroundColor(Color.RED);
+        headToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HeadTestActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
